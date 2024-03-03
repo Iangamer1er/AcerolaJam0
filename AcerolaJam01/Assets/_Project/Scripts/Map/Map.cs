f6444a41700e9f86551244b8196ee91c436d763e
Iangamer1er <Iangamer1er@gmail.com>
2024-03-03 03:45:40 -0500

    start of the map script

--- AcerolaJam01/Assets/_Project/Scripts/Map/Map.cs
+++ AcerolaJam01/Assets/_Project/Scripts/Map/Map.cs
@@ -5,6 +5,36 @@ using UnityEngine;
 
 public class Map : ValidatedMonoBehaviour
 {
-    [SerializeField, Range(0, 50)] int numberEncounters = 10;
-    // [SerializeField, ] 
+    [SerializeField, Range(0, 50)] int nbEncounters = 10;
+    [SerializeField, Range(0, 10)] int nbForks = 1;
+    [SerializeField, Range(2, 4)] int maxForkSplits = 3;
+    private int currentNbEncounters;
+    private int currentNbForks;
+
+    private void Awake() {
+        currentNbEncounters = nbEncounters;
+        currentNbForks = nbForks;
+    }
+
+    private void MakeEncounters(){
+        for (int i = 0; i < nbEncounters; i++){
+            
+        }
+    }
+
+    private void MakeForksPath(){
+        if(currentNbForks < 1) return;
+
+    }
+
+    private void CalculateForkChance(){
+        if(!checkNbEncounters()) return;
+        currentNbForks--;
+        int forkSplits = Random.Range(2, maxForkSplits);
+    }
+
+    private bool checkNbEncounters(){
+        return (float)currentNbEncounters/(float)nbEncounters>= Random.Range(0, 1) ||
+            nbEncounters - currentNbEncounters < 3;
+    }
 }
